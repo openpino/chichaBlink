8fcb8ce5c41455384051f0eb7e1a76644721c856
Suppachai.m <openpino@hotmail.com>
2018-01-24 11:31:45 +0700

    Pin code and Delay time

--- chichaBlink.ino
+++ chichaBlink.ino
@@ -1,14 +1,17 @@
+//ส่วนของการประกาศตัวแปร
 
-
+int myOutput = D0;
+int myDelayTime = 2000;//ค่าของเวลาจะมีหน่วยเป็น มิลลิวินาที
+//ส่วนของ setup คือส่วนที่กำหนดค่าคงที่ที่จะใช้ในโค้ตจะทำงานครั้งแรกครั้งเดียว
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(16, OUTPUT);
+  pinMode(myOutput, OUTPUT);
 }
 
-// the loop function runs over and over again forever
+//ส่วนของ loop จะทำงานวนตามสัญญาณนาฬิกาตลอดเวลา
 void loop() {
-  digitalWrite(16, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(16, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  digitalWrite(myOutput, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(myDelayTime);                       // wait for a second
+  digitalWrite(myOutput, LOW);    // turn the LED off by making the voltage LOW
+  delay(myDelayTime);                       // wait for a second
 }
